e11d5469f9a81ca340121220306ff5f7b5beaf1c
jaeunP <okn2432@gmail.com>
2023-01-11 16:53:46 +0900

    example

---
+++ src/main/java/spring/introduction/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package spring.introduction.repository;
+
+import spring.introduction.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
