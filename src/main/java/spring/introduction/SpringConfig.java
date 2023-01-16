a3cff84e26dbc13f52ae9176660abf098e00b959
jaeunP <okn2432@gmail.com>
2023-01-16 13:19:48 +0900

    JDBC

--- src/main/java/spring/introduction/SpringConfig.java
+++ src/main/java/spring/introduction/SpringConfig.java
@@ -1,13 +1,23 @@
 package spring.introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import spring.introduction.repository.JdbcMemberRepository;
 import spring.introduction.repository.MemberRepository;
 import spring.introduction.repository.MemoryMemberRepository;
 import spring.introduction.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
--- src/main/java/spring/introduction/controller/MemberController.java
+++ src/main/java/spring/introduction/controller/MemberController.java
@@ -24,7 +24,7 @@ public class MemberController {
         return "members/createMemberForm";
     }
 
-    @PostMapping(value = "/members/new")
+    @PostMapping("/members/new")
     public String create(MemberForm form) {
         Member member = new Member();
         member.setName(form.getName());
