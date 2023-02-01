19bd0f4f999c6120e5085a657e879e00b261f4fa
jaeunP <okn2432@gmail.com>
2023-02-01 14:25:42 +0900

    infleran-core

---
+++ core/src/main/java/inflearn/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package inflearn.core.discount;
+
+import inflearn.core.member.Grade;
+import inflearn.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
