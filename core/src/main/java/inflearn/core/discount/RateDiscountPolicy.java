d34852afc4d463320fe3a9b429e2834b6227ff06
jaeunP <okn2432@gmail.com>
2023-02-13 22:43:27 +0900

    infleran-core

--- core/src/main/java/inflearn/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/inflearn/core/discount/RateDiscountPolicy.java
@@ -2,7 +2,9 @@ package inflearn.core.discount;
 
 import inflearn.core.member.Grade;
 import inflearn.core.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
