6ecd0c1244a2f91838b7fe806893dc7a0f94e3ea
jaeunP <okn2432@gmail.com>
2023-04-03 23:17:10 +0900

    jpashop

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -1,13 +1,10 @@
 package jpabook.jpashop.api;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
-import java.util.List;
-import java.util.stream.Collectors;
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
@@ -39,5 +36,16 @@ public class MemberApiController {
         public CreateMemberResponse(Long id) {
             this.id = id;
         }
+    }
+    /**
+     * 등록 V2: 요청 값으로 Member 엔티티 대신에 별도의 DTO를 받는다.
+     */
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid
+                                             CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
     }
 }
