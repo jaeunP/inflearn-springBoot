e9da09b5f7f3ee298816b0402822d9bf230cfc96
jaeunP <okn2432@gmail.com>
2023-04-20 22:35:10 +0900

    jpashop

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -33,17 +33,20 @@ public class OrderApiController {
      * - 엔티티가 변하면 API 스펙이 변한다.
      * - 트랜잭션 안에서 지연 로딩 필요
      * - 양방향 연관관계 문제
-     * <p>
+     *
      * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
      * - 트랜잭션 안에서 지연 로딩 필요
+     *
      * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
      * - 페이징 시에는 N 부분을 포기해야함(대신에 batch fetch size? 옵션 주면 N -> 1 쿼리로 변경
      * 가능)
-     * <p>
+     *
      * V4. JPA에서 DTO로 바로 조회, 컬렉션 N 조회 (1 + N Query)
      * - 페이징 가능
+     *
      * V5. JPA에서 DTO로 바로 조회, 컬렉션 1 조회 최적화 버전 (1 + 1 Query)
      * - 페이징 가능
+     *
      * V6. JPA에서 DTO로 바로 조회, 플랫 데이터(1Query) (1 Query)
      * - 페이징 불가능...
      */
