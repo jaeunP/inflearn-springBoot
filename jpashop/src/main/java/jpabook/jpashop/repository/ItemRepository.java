93c8989ddd385a90d51e822c09e3151228420102
jaeunP <okn2432@gmail.com>
2023-04-23 00:32:03 +0900

    jpashop

--- jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -1,29 +1,7 @@
 package jpabook.jpashop.repository;
+
 import jpabook.jpashop.domain.item.Item;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
-import javax.persistence.EntityManager;
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-@RequiredArgsConstructor
-public class ItemRepository {
-
-    private final EntityManager em;
-
-    public void save(Item item) {
-        if (item.getId() == null) {
-            em.persist(item);
-        } else {
-            em.merge(item);
-        }
-    }
-
-    public Item findOne(Long id) {
-        return em.find(Item.class, id);
-    }
-
-    public List<Item> findAll() {
-        return em.createQuery("select i from Item i",Item.class).getResultList();
-    }
+public interface ItemRepository extends JpaRepository<Item, Long> {
 }
